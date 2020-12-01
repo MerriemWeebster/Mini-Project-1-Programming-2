645c4f1c39439bdd241cc7fe3f7c99bc0ca5b3d2
MerriemWeebster <daniyalkhan2000@users.noreply.github.com>
2020-12-01 21:10:56 +0400

    Main and Test Main Comb

--- main.cpp
+++ main.cpp
@@ -29,7 +29,26 @@ int main()
 	cout << "Standard Deviation of Matrix b: " << stdev(b) << endl;
 	cout << "Sum of Matrix c: " << SumMatrixElements(c) << endl;
 	cout << "Standard Deviation of Matrix c: " << stdev(c) << endl;
-   cout<<"---------------------------------Testing Out All Memebers Of The Matrix Class-------------------------------------------";
+
+    cout<<"\n---------------------------------Testing Out All Memebers Of The Matrix Class-------------------------------------------\n\n";
+
+    Matrix A(2, 2); //testing default constructor
+	cout << "Enter Matrix A : ";
+	cin >> A; //testing extraction operator
+	cout << "\nMatrix A : " << endl << A << endl;
+	Matrix B(A); //testing copy constructor
+	cout << "Matrix -B : " << endl << -B << endl; //testing negation operator
+	cout << "Element at row 1, column 0 of Matrix A : " << A(1, 0) << endl; //testing rvalue indices operator
+	A(0, 0) = 5; //testing lvalue indices operator
+	cout << "\nMatrix A after A(0, 0) = 5 : " << endl << A << endl;
+	Matrix C = A; //testing assignment operator
+	cout << "C = A gives us : " << endl << C << endl;
+	--C; //testing decrement operator
+	cout << "--C = " << endl << C << endl;
+	B += A; //testing shorthand addition operator
+	cout << "B += A : " << endl << B << endl;
+	C -= B; //testing shorthand subtraction operator
+	cout << "C -= B : " << endl << C << endl;
 	
     return 0;
 }
