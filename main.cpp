de4bfdb2fde85bb3807a1e963d70d53ccd8b691a
Pr3mR4jendran <75168548+Pr3mR4jendran@users.noreply.github.com>
2020-12-03 13:15:58 +0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,23 +5,23 @@
 #include "Matrix.h"
 using namespace std;
 
-double SumMatrixElements (const Matrix &); //Decleration of sum func
-double stdev (const Matrix &); //Decleration of standard dev func
+double SumMatrixElements(const Matrix&); //Decleration of sum func
+double stdev(const Matrix&); //Decleration of standard dev func
 
 int main()
 {
-	cout<<"---------------------------------PROBLEM 1-------------------------------------------\n\n";
-    cout<<"\n---------------------------------Testing Out All Memebers Of The Matrix Class-------------------------------------------\n\n";
+	cout << "---------------------------------PROBLEM 1-------------------------------------------\n\n";
+	cout << "--------------------Testing Out All Members Of The Matrix Class----------------------\n\n";
 
-    Matrix A(2, 2); //testing default constructor
-    Matrix B(A); //testing copy constructor
-    cout << "Enter Matrix A (Size:2x2):";
+	Matrix A(2, 2); //testing default constructor
+	Matrix B(A); //testing copy constructor
+	cout << "Enter Matrix A (Size:2x2):";
 	cin >> A; //testing extraction operator
-	cout<<"Enter Matrix B (Size:2x2):";
-	cin>> B;
+	cout << "Enter Matrix B (Size:2x2):";
+	cin >> B;
 	cout << "\nMatrix A : " << endl << A << endl;
 	cout << "\nMatrix B : " << endl << B << endl;
-	cout<<"Matrix A After A-B:"<<endl <<(A-B)<<endl; // Testing The subtraction operator
+	cout << "Matrix A After A-B:" << endl << (A - B) << endl; // Testing The subtraction operator
 	cout << "Matrix -B : " << endl << -B << endl; //testing negation operator
 	cout << "Element at row 1, column 0 of Matrix A : " << A(1, 0) << endl; //testing rvalue indices operator
 	A(0, 0) = 5; //testing lvalue indices operator
@@ -32,30 +32,30 @@ int main()
 	cout << "--C = " << endl << C << endl;
 	++C; //Testing The Increment Operator
 	cout << "++C = " << endl << C << endl;
-	cout << "A==B = "<<endl<<(A==B)<<endl; // Testing The Equality Operator
-	cout << "A!=B = "<<endl<<(A!=B)<<endl; // Testing The Inequality Operator
-	cout << "A * B = "<<endl<<(A*B)<<endl; // Testing The Matrix Multiplication Operator
-	cout<<" 5 * A = "<<endl<<(A*5)<<endl; // Testing The Scalar Multiplication Operator
+	cout << "A==B = " << endl << (A == B) << endl; // Testing The Equality Operator
+	cout << "A!=B = " << endl << (A != B) << endl; // Testing The Inequality Operator
+	cout << "A * B = " << endl << (A * B) << endl; // Testing The Matrix Multiplication Operator
+	cout << " 5 * A = " << endl << (A * 5) << endl; // Testing The Scalar Multiplication Operator
 	B += A; //testing shorthand addition operator
 	cout << "B += A : " << endl << B << endl;
 	C -= B; //testing shorthand subtraction operator
 	cout << "C -= B : " << endl << C << endl;
-    
-	cout<<"\\n---------------------------------PROBLEM 2-------------------------------------------\n\n";
+
+	cout << "\\n--------------------------------PROBLEM 2----------------------------------------\n\n";
 
 	//Create 3 different matrices
 	Matrix a;
 	Matrix b(3, 3);
 	Matrix c(b);
-	
+
 	//Take in values for all 3
-	cout<<"Enter The Matrix a(Size:2x3):";
+	cout << "Enter The Matrix a(Size:2x3):";
 	cin >> a;
-	cout<<"Enter The Matrix b(Size:3x3):";
+	cout << "Enter The Matrix b(Size:3x3):";
 	cin >> b;
-	cout<<"Enter The Matrix c(Size:3x3):";
+	cout << "Enter The Matrix c(Size:3x3):";
 	cin >> c;
-	
+
 	//Output sum and standard deviation for all 3 matrices
 	cout << "Sum of Matrix a: " << SumMatrixElements(a) << endl;
 	cout << "Standard Deviation of Matrix a: " << stdev(a) << endl;
@@ -63,40 +63,43 @@ int main()
 	cout << "Standard Deviation of Matrix b: " << stdev(b) << endl;
 	cout << "Sum of Matrix c: " << SumMatrixElements(c) << endl;
 	cout << "Standard Deviation of Matrix c: " << stdev(c) << endl;
-    
-    return 0;
+
+	return 0;
 }
 
-double SumMatrixElements (const Matrix &m) //Implementation of sum func
-{
-    double sum = 0; //Declare sum double
-    int r = m.get_rows(), c = m.get_cols(); //Store length of rows and cols in int r and c
 
-    for(int i = 0; i < r; i++)
-    {
-        for(int j = 0; j < c; j++)
-        {
-            sum += m(i, j); //Loop through all elements and add them to sum
-        }
-    }
-    
-    return sum; //Return sum
+
+
+double SumMatrixElements(const Matrix& m) //Implementation of sum func
+{
+	double sum = 0; //Declare sum double
+	int r = m.get_rows(), c = m.get_cols(); //Store length of rows and cols in int r and c
+
+	for (int i = 0; i < r; i++)
+	{
+		for (int j = 0; j < c; j++)
+		{
+			sum += m(i, j); //Loop through all elements and add them to sum
+		}
+	}
+
+	return sum; //Return sum
 }
 
-double stdev (const Matrix &m) //Implementation of standard dev func
+double stdev(const Matrix& m) //Implementation of standard dev func
 {
-    int r = m.get_rows(), c = m.get_cols(); //Store length of rows and cols in int r and c
-    double avg = SumMatrixElements(m) / (r * c), //Calculate average
-    devSum = 0; //Used in loop for numerator of standard dev
-    
-    for(int i = 0; i < r; i++)
-    {
-        for(int j = 0; j < c; j++)
-        {
-            devSum += pow((m(i, j) - avg), 2); //Get the numerator (deviation sum) by going through each row and col
-        }
-    }
-    
-    double standardDev = sqrt(devSum / (r * c - 1)); //Standard Dev calculated using formula
-    return standardDev; //Return standardDev
+	int r = m.get_rows(), c = m.get_cols(); //Store length of rows and cols in int r and c
+	double avg = SumMatrixElements(m) / (r * c), //Calculate average
+		devSum = 0; //Used in loop for numerator of standard dev
+
+	for (int i = 0; i < r; i++)
+	{
+		for (int j = 0; j < c; j++)
+		{
+			devSum += pow((m(i, j) - avg), 2); //Get the numerator (deviation sum) by going through each row and col
+		}
+	}
+
+	double standardDev = sqrt(devSum / (r * c - 1)); //Standard Dev calculated using formula
+	return standardDev; //Return standardDev
 }
